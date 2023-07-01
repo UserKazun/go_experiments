99787bb646a28e644730c703508bf7a0b9bcd15a
UserKazun <developer.kazu0941@gmail.com>
2023-07-01 20:49:20 +0900

    create server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Hello World!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8000",
+		Handler: &handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		_ = fmt.Errorf("%v", err)
+		return
+	}
 }
