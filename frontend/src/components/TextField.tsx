1e41eabebf4dea0943759753260ee601bf9c7504
UserKazun <developer.kazu0941@gmail.com>
2023-07-02 19:36:58 +0900

    delete frontend

--- frontend/src/components/TextField.tsx
+++
@@ -1,8 +0,0 @@
-const TextField = () => {
-    return (
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-               placeholder="Add Todo"></input>
-    )
-}
-
-export default TextField
