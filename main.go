b1ffaf122f2f4bb6bd0dcaceff5ad58eeb7a71a9
UserKazun <developer.kazu0941@gmail.com>
2023-07-08 16:19:55 +0900

    add experiments

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"math/rand"
 	"net/http"
 	"time"
 )
-
-type Todo struct {
-	Data []string `json:"data"`
-}
-
-func APIAddTodo(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
-
-	w.WriteHeader(200)
-}
-
-func APIGetTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	todos := Todo{Data: []string{"job", "programming"}}
-
-	res, _ := json.Marshal(todos)
-	_, err := w.Write(res)
-	if err != nil {
-		return
-	}
-}
 
 func tempTest(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("views/index.html")
@@ -45,14 +19,54 @@ func tempTest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func experimentsHello(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:  "experiments_token",
+		Value: "hello",
+		Path:  "/",
+	}
+
+	http.SetCookie(w, &cookie)
+}
+
+func experimentsGood(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:  "experiments_token",
+		Value: "good",
+		Path:  "/",
+	}
+
+	http.SetCookie(w, &cookie)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	token, err := r.Cookie("experiments_token")
+	if err != nil {
+		fmt.Fprintln(w, "cannot get the cookie")
+		return
+	}
+
+	// If there is GOOD, then HELLO is not shown.
+	if token.Value == "hello" {
+		fmt.Fprintln(w, "get cookie")
+		if err != nil {
+			return
+		}
+
+		return
+	} else {
+		fmt.Fprintln(w, "cannot get 'hello' cookie")
+	}
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8000",
 	}
-	http.HandleFunc("/api/todo", APIAddTodo)
-	http.HandleFunc("/api/todos", APIGetTodo)
-
 	http.HandleFunc("/sample", tempTest)
+	http.HandleFunc("/experiments/hello", experimentsHello)
+	http.HandleFunc("/experiments/good", experimentsGood)
+	http.HandleFunc("/hello", hello)
 
 	err := server.ListenAndServe()
 	if err != nil {
